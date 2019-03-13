1ba35f6bd3585d9a12adffc5a9de3689a33fa8dd
lugy90 <naitianzong@outlook.com>
2019-03-13 18:53:25 +0800

    images

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
     entry:  './app/index.js', // 入口文件
     output: {
       path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
-      filename: "bundle.js" // 打包后输出文件的文件名
+      filename: "bundle.js", // 打包后输出文件的文件名
+      publicPath: 'build/' // 知道如何寻找资源
     },
     module: {
         rules: [
@@ -14,6 +15,23 @@ module.exports = {
                 use: 'babel-loader',
             // 不包括路径
                 exclude: /node_modules/
+            },
+            {
+            // 图片格式正则
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    // 配置 url-loader 的可选项
+                    options: {
+                    // 限制 图片大小 10000B，小于限制会将图片转换为 base64格式
+                      limit: 10000,
+                    // 超出限制，创建的文件格式
+                    // build/images/[图片名].[hash].[图片格式]
+                      name: 'images/[name].[hash].[ext]'
+                   }
+                  }
+                ]
             }
         ]
     },
