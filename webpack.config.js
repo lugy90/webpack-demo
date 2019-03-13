b180ead88fc67b93fa8102fa268ac39a3f2a9542
lugy90 <naitianzong@outlook.com>
2019-03-13 19:12:36 +0800

    process css

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,17 @@ module.exports = {
                    }
                   }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                           }
+                    }
+                ]
             }
         ]
     },
